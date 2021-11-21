d1d07b74dc6f74010e3ee89dd7c38e98e8826477
Grzegorz <g.ratajczak97@gmail.com>
2021-11-21 10:27:31 +0100

    added virtual machine screen

--- DataAccess/db-context.js
+++ DataAccess/db-context.js
@@ -17,6 +17,33 @@ class PeopleDbContext {
         this.log("getPeople function - done")
         return result.recordset;
     }
+
+    async removePerson(id){
+        this.log("removePerson function - run")
+        const connection = await new sql.ConnectionPool(this.config).connect();
+        const request = new sql.Request(connection);
+        const result = await request.query('delete from People where PersonId = ' + id);
+        this.log("removePerson function - done")
+        return result.recordset;
+    }
+
+    async getPerson(id){
+        this.log("property function - run")
+        const connection = await new sql.ConnectionPool(this.config).connect();
+        const request = new sql.Request(connection);
+        const result = await request.query('select * from People where PersonId = ' + id);
+        this.log("property function - done")
+        return result.recordset;
+    }
+
+    async addPerson(options){
+        this.log("addPerson function - run")
+        const connection = await new sql.ConnectionPool(this.config).connect();
+        const request = new sql.Request(connection);
+        const result = await request.query('Insert into People values ( '+ options.name +', '+ options.surname +', '+ options.phone +');');
+        this.log("addPerson function - done")
+        return result.recordset;
+    }
 }
 
 module.exports = PeopleDbContext;
--- PeopleHttpTrigger/index.js
+++ PeopleHttpTrigger/index.js
@@ -6,6 +6,15 @@ module.exports = async function (context, req) {
     await common.functionWrapper(context, req, async (body) => {
         const connectionString = process.env['PeopleDb'];
         const peopleDb = new PeopleDbContext(connectionString, context.log);
-        body.people = await peopleDb.getPeople();
+        if(req.method === 'GET'){
+
+            body.people = await peopleDb.getPeople();
+
+        }else if(req.method === 'POST'){
+            context.log(req);
+
+            body.people = await peopleDb.addPerson(req.body.newperson);
+            
+        }
     });
 };
